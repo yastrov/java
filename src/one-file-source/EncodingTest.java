57ae3a0b612ee9e0ba00ae98a5e2a74f5d4c2a1a
Yuri Astrov <yuriastrov@gmail.com>
2014-06-28 22:40:07 +0400

    Fix import.

--- src/one-file-source/EncodingTest.java
+++ src/one-file-source/EncodingTest.java
@@ -1,9 +1,17 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * Example for work with Encoding.
